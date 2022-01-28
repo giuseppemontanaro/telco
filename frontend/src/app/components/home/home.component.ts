0700e688b24dcf77e038b5b575522a189c673453
Giuseppe Montanaro <g.montanaro@reply.it>
2022-01-28 17:36:30 +0100

    [Frontend]
    migliorato layout user

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -6,6 +6,11 @@ import { OrderDaoService } from 'src/app/services/order-dao.service';
 import { Order } from 'src/app/models/order';
 import { ModelService } from 'src/app/services/model.service';
 import { Const } from 'src/app/shared/constants';
+import { Service } from 'src/app/models/service';
+import { FixedInternet } from 'src/app/models/fixedInternet';
+import { FixedPhone } from 'src/app/models/fixedPhone';
+import { MobileInternet } from 'src/app/models/mobileInternet';
+import { MobilePhone } from 'src/app/models/mobilePhone';
 
 @Component({
   selector: 'app-home',
@@ -25,7 +30,6 @@ export class HomeComponent implements OnInit {
     const user = this.model.getBean(Const.USER);
     this.orderDao.getRejectedOrders(user)
       .subscribe(rejected => this.rejected = rejected);
-    console.log(this.rejected);
   }
 
   goToBuyPage(): void {
@@ -35,5 +39,14 @@ export class HomeComponent implements OnInit {
   toConfirmation(i: number): void {
     this.model.putBean(Const.CHOSEN_PACKAGE, this.rejected[i].package);
     this.router.navigate(['confirmation'])
+  }
+
+  printDetails(service: Service) {
+    if (service.title == 'Mobile Internet' || service.title == 'Fixed Internet') {
+      return (service as MobileInternet).gigabytes + ' GB - $' + (service as MobileInternet).gigabytesExtraFee + ' extra fee'
+    } else if (service.title == 'Mobile Phone') {
+      return (service as MobilePhone).sms + ' SMS - $' + (service as MobilePhone).smsExtraFee + ' extra fee ' + (service as MobilePhone).minutes + ' minutes - $' + (service as MobilePhone).smsExtraFee + ' extra fee'
+    }
+    return ''
   }
 }
