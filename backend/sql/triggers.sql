82c5c0979f6aa36e40b641ff4c0f206edc4731b7
Giuseppe Montanaro <g.montanaro@reply.it>
2022-02-10 21:53:11 +0100

    modifiche trigger

--- backend/sql/triggers.sql
+++ backend/sql/triggers.sql
@@ -1,53 +1,75 @@
 -- mat view
 CREATE TABLE service_package_view(service_pkg_id INT, total INT, total_val_per_12 INT, total_val_per_24 INT, total_val_per_36 INT, sales_pkg_only DOUBLE, sales_with_opt_prods DOUBLE, avg_prods DOUBLE);
 
-CREATE TABLE insolvent_users_view(user ddl);
+CREATE TABLE insolvent_users_view(id int AUTO_INCREMENT, username VARCHAR(45), email VARCHAR(45), PRIMARY KEY(id));
 
-CREATE TABLE suspended_order_view(order ddl);
+CREATE TABLE suspended_order_view(id int AUTO_INCREMENT, total float, subscription_date datetime, date datetime, PRIMARY KEY(id));
 
-CREATE TABLE alert_view(user_id INT, username VARCHAR(30), email VARCHAR(30), order_id INT, amount DOUBLE, last_rejection_date DATETIME);
+CREATE TABLE alert_view(user_id INT, username VARCHAR(45), email VARCHAR(45), order_id INT, amount DOUBLE, last_rejection_date DATETIME);
 
-CREATE TABLE best_seller_view(id INT, name VARCHAR(50));
+CREATE TABLE best_seller_view(name VARCHAR(50), purchases int);
 
 
 -- trigger
-CREATE TRIGGER service_package_trigger 
-AFTER INSERT ON order
+CREATE TRIGGER service_package_trigger  
+AFTER INSERT ON purchase 
+FOR EACH ROW  
+DELETE FROM service_package_view spw;
+INSERT INTO service_package_view   
+SELECT s.id as service_pkg_id, count(*) as total,
+    count(case p.month_number when 12 then 1 else null end) as total_val_per_12,
+    count(case p.month_number when 24 then 1 else null end) as total_val_per_24,
+    count(case p.month_number when 36 then 1 else null end) as total_val_per_36,
+    sum(p.monthly_fee) as sales_pkg_only,   
+    sum(p.monthly_fee) + sum(op.monthly_fee) sales_with_opt_prods,   
+    count(*) / count(distinct s.id) avg_prods  
+    FROM purchase o, service_package s, product op, validity_period p, order_product opo  
+    WHERE o.service_pkg_fk = s.id and opo.product_fk = op.id and opo.order_fk = o.id and o.validity_period_fk = p.id  
+    GROUP BY s.id
+
+
+DELIMITER $$
+CREATE TRIGGER insolvent_users_trigger 
+AFTER UPDATE ON Telco.user 
 FOR EACH ROW 
-DELETE FROM service_package_view spw WHERE spw.service_pkg_id = new.service_pkg_id
-INSERT INTO service_package_view 
-	SELECT s.id as service_pkg_id, count(*) as total, 
-		count(case x. when 12 then 1 else null end) as total_val_per_12,
-		count(case x. when 24 then 1 else null end) as total_val_per_24,
-		count(case x. when 36 then 1 else null end) as total_val_per_36,
-		sum(s.cost) as sales_pkg_only,
-		sum(s.cost) + sum(op.cost) sales_with_opt_prods,
-		count(*) / count(distinct s.service_pkg_id) avg_prods
-	FROM order o, service_package s, optional_products op, validity_period p
-	WHERE o.service_pkg_id = s.id and o.optional_products_fk = op.id and o.validity_period_fk = p.id
-	GROUP BY s.id;
+BEGIN
+	IF (new.isInsolvent = true)     
+	THEN INSERT INTO insolvent_users_view VALUES (new.id, new.username, new.email);  
+	END IF; 
+END$$
+DELIMITER;
 
 
-CREATE TRIGGER insolvent_users_trigger
-AFTER UPDATE ON user
-FOR EACH ROW
-WHEN new.insolvent = true
-INSERT INTO insolvent_users_view VALUES (new.id, new.username, new.email);
+DELIMITER $$
+CREATE TRIGGER suspended_order_trigger 
+AFTER INSERT ON purchase 
+FOR EACH ROW 
+BEGIN  
+	IF (new.isRejected = true)     
+    THEN INSERT INTO suspended_order VALUES(id, total, subscription_date, date);  
+    END IF; 
+END$$
+DELIMITER ;
 
 
-CREATE TRIGGER suspended_order_trigger
-AFTER UPDATE ON order
-FOR EACH ROW
-WHEN new.is_rejected = true
-INSERT INTO suspended_order VALUES (row);
+CREATE TRIGGER alert_trigger 
+AFTER INSERT ON purchase 
+FOR EACH ROW INSERT INTO alert_view
+   SELECT u.id, u.username, u.email, sum(o.cost), max(o.date)
+   FROM purchase o, user u
+   WHERE o.user_fk = u.id and u.insolvent = true
+   GROUP BY u.id
+   HAVING count(*) >= 3
 
-user_id INT, username VARCHAR(30), email VARCHAR(30), order_id INT, amount DOUBLE, last_rejection_date DATETIME
-CREATE TRIGGER alert_trigger
-AFTER UPDATE ON order
-FOR EACH ROW
-INSERT INTO alert_view 
-	SELECT u.id, u.username, u.email, sum(o.cost), max(o.date)
-	FROM order o, user u
-	WHERE o.user_fk = u.id and u.insolvent = true
-	GROUP BY u.id
-	HAVING count(*) >= 3;
+
+CREATE TRIGGER bestseller_trigger
+AFTER UPDATE ON purchase 
+FOR EACH ROW 
+DELETE FROM best_seller_view bsw;
+INSERT INTO best_seller_view
+  SELECT p.name, count(*) as num_orders
+  FROM purchase o, order_product op, product p
+  WHERE op.order_fk = o.id and op.product_fk = p.id
+  GROUP BY p.name
+  ORDER BY num_orders DESC
+  LIMIT 1
